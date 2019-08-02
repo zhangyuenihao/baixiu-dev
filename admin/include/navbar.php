ff1b7eeb102f5218052499d883a8f7829264946e
zhangyuenihao <1914759018@qq.com>
2019-08-02 19:33:18 +0800

    第一次提交

---
+++ admin/include/navbar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<nav class="navbar">
+    <button class="btn btn-default navbar-btn fa fa-bars"></button>
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="profile.php"><i class="fa fa-user"></i>个人中心</a></li>
+        <li><a href="login.php"><i class="fa fa-sign-out"></i>退出</a></li>
+    </ul>
+</nav>
+</body>
+</html>
---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+*我们项目中用到的配置信息
+*/
+/**
+*数据库主机
+*/
+define('DB_HOST','localhost');
+/**
+*数据库用户名
+*/
+define('DB_USER','root');
+/**
+*数据库密码
+*/
+define('DB_PASS','123456');
+/**
+*数据库名字
+*/
+define('DB_NAME','baixiu-dev');
+//  这种只有服务端代码的 PHP 文件应该去除结尾处的 '?'，防止输出内容
+
+
